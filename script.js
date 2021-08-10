4a88b46e4501ed6e129906ebc396e8f1baa76caa
SvetLana203 <shirapovauu@gmail.com>
2021-08-10 09:50:19 -0500

    project

---
+++ script.js
@@ -0,0 +1,13 @@
+const cardsAll = document.querySelectorAll('.cards')
+console.log(cardsAll)
+
+flipCard = () => {
+  cardsAll.classList.toggle('flip') ///????
+}
+
+cardsAll.forEach((card) => card.addEventListener('click', flipCard))
+
+//   cardMatches = () => {
+//  if()
+
+//   }
