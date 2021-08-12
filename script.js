d619aa2693463338823603fa7cc42dad8dae6253
SvetLana203 <shirapovauu@gmail.com>
2021-08-11 23:24:11 -0500

    game

--- script.js
+++ script.js
@@ -1,23 +1,55 @@
+let cardArray = []
+let cardFirst
+let cardSecond
+
 const cardsAll = document.querySelectorAll('.a_card')
+
 console.log(cardsAll)
 
-function flipCard() {
-  this.classList.toggle('flip')
+function flipCard(card) {
+  card.classList.toggle('flip')
 }
 
-cardsAll.forEach((card) => card.addEventListener('click', flipCard))
+cardsAll.forEach((card, index) =>
+  card.addEventListener('click', () => {
+    flipCard(card)
+    // cardArray.push(card.getAttribute('value'))
+
+    console.log(card.getAttribute('value'))
+    console.log(cardArray)
+    cardMatches(card)
+  })
+)
 console.log(flipCard)
 
 ///////matching cards//////
 
-// cardMatches = () => {
-//  if()
+const cardMatches = (card) => {
+  if (!cardFirst) {
+    console.log('first card clicked')
+    cardFirst = card.getAttribute('value')
+  } else {
+    cardSecond = card.getAttribute('value')
+    console.log('second card clicked')
+    if (cardFirst === cardSecond) {
+      console.log('Match')
+      freezeCards(card)
+      cardFirst = null // reset// we are not comparing any cards
+      cardSecond = null
+    } else {
+      console.log(' Cards are not matched')
+      //
+      cardFirst = null
+      cardSecond = null
+    }
+  }
+}
+const freezeCards = (card) => {
+  card.removeEventListener('click', flipCard)
+  // cardSecond.removeEventListener('click', flipCard)
+}
 
-//   }
-// const cards = document.querySelectorAll('.a_card')
-// console.log(cards)
-// function flipCard() {
-//   this.classList.toggle('flip')
-// }
+/////shuffling cards////
 
-// cards.forEach((card) => card.addEventListener('click', flipCard))
+// Math.floor(Math.random() * ???)
+//
